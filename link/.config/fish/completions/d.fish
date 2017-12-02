2bef0e438cda76904da28f6115cf110a488bd889
Christoph Menke <christoph@menke.co>
2017-12-02 12:22:18 +0100

    fish: Fix completions for `d`, `w` directory shortcuts

--- link/.config/fish/completions/d.fish
+++ link/.config/fish/completions/d.fish
@@ -1,2 +1,2 @@
 complete --command d --erase
-complete --command d --exclusive --arguments '(__fish_complete_directories (eval \"$GDRIVE\"))'
+complete --command d --exclusive --arguments '(__fish_complete_directories $GDRIVE/(commandline -ct) | sed -e "s#$GDRIVE/##")'
--- link/.config/fish/completions/w.fish
+++ link/.config/fish/completions/w.fish
@@ -1,2 +1,2 @@
 complete --command w --erase
-complete --command w --exclusive --arguments '(__fish_complete_directories (eval $WORKSPACE))'
+complete --command w --exclusive --arguments '(__fish_complete_directories $WORKSPACE/(commandline -ct) | sed -e "s#$WORKSPACE/##")'
